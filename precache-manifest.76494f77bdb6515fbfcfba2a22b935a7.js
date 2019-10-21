3a7fcb699d22ed261d934af5ddf61f4515ce6296
PinoyFreeCoder <geek.korky@gmail.com>
2019-10-21 21:10:01 +0800

    Updates

---
+++ precache-manifest.76494f77bdb6515fbfcfba2a22b935a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de485dda9c0dca148531a20d8541bf23",
+    "url": "/ReactApp/index.html"
+  },
+  {
+    "revision": "5a7bed81fd415f14b28b",
+    "url": "/ReactApp/static/css/main.c2f54619.chunk.css"
+  },
+  {
+    "revision": "525fa2bed024c13b66f8",
+    "url": "/ReactApp/static/js/2.03486ddb.chunk.js"
+  },
+  {
+    "revision": "5a7bed81fd415f14b28b",
+    "url": "/ReactApp/static/js/main.84c1602a.chunk.js"
+  },
+  {
+    "revision": "fc0f5060534cc39cd028",
+    "url": "/ReactApp/static/js/runtime-main.5ff86cc0.js"
+  }
+]);
