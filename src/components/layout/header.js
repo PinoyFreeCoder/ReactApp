da549c27c3b07a2a41727c73cd4d4aabe550f6c2
PinoyFreeCoder <geek.korky@gmail.com>
2019-10-21 21:11:28 +0800

    second commit

---
+++ src/components/layout/header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header style={headerStyle}>
+      <h1>My Todolist</h1>
+      <Link to="/ReactApp" style={LinkStyle}>
+        Home
+      </Link>{" "}
+      |{" "}
+      <Link to="/about" style={LinkStyle}>
+        About
+      </Link>
+    </header>
+  );
+}
+
+const headerStyle = {
+  background: "Teal",
+  color: "#fff",
+  textAlign: "center",
+  padding: "10px"
+};
+
+const LinkStyle = {
+  color: "#fff",
+  textDecoration: "none"
+};
+
+export default Header;
